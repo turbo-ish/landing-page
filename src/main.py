40532238a6529f18d4411211ad712a55810c707f
NikolaiSavulkin <nikolai.savulkin@ru.nl>
2025-10-02 08:40:14 +0200

    no clue what changes are there, needed to pull

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from dbhandler import add_vote_record, add_loc_record
 
 app = Flask(__name__)
 
+
 @app.route("/<int:qr_id>", methods=['GET', 'POST'])
 def landing(qr_id: int):
     """
@@ -26,6 +27,7 @@ def landing(qr_id: int):
 
     return resp
 
+
 @app.route('/<int:qr_id>/qr', methods=['GET'])
 def qrcode(qr_id: int):
     data = "movetogether.now/" + str(qr_id)
@@ -37,6 +39,7 @@ def qrcode(qr_id: int):
 
     return '<img src="data:image/jpeg;base64,' + img_bin + '"/>'
 
+
 @app.route("/setqrloc", methods=['GET', 'POST'])
 def set_qr_loc():
     if request.method == 'POST':
@@ -45,12 +48,12 @@ def set_qr_loc():
     return render_template('set_qr_loc.html')
 
 
-
 if __name__ == '__main__':
     db = sqlite3.connect("../myfuckingdb.db", check_same_thread=False)
     cur = db.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS qr2loc (id INTEGER PRIMARY KEY AUTOINCREMENT, lat FLOAT, lng FLOAT);")
-    cur.execute("CREATE TABLE IF NOT EXISTS vote2qr (id INTEGER PRIMARY KEY AUTOINCREMENT, response TEXT, qr_id INTEGER);")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS vote2qr (id INTEGER PRIMARY KEY AUTOINCREMENT, response TEXT, qr_id INTEGER);")
     cur.close()
 
     app.run(host="127.0.0.1", port=8000, debug=True)
